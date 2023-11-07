2a44e28d8e5eda45fd55f6b3fac8bd13ce915f18
Bhanuprakashreddyv <v.bhanureddy184@gmail.com>
2023-11-07 08:25:41 +0000

    Get data from API and ready to deploy.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import Navbar from "./Components/Navbar";
 import NewsBoard from "./Components/NewsBoard";
-import NewsItem from "./Components/NewsItem";
+
 
 const App = () => {
+  const [category, setCategory] = useState ("general")
   return (
     <div>
-      <Navbar />
-      <NewsBoard />
-      <NewsItem />
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+      
     </div>
   );
 }
